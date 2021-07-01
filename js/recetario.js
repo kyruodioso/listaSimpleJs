c42e569b26a76f918512b745d7248bb0ff7d2ede
Cristian Bejarano <kanu_nirvana@hotmail.com>
2021-07-01 09:58:38 -0300

    mejorando las respuestas

--- js/recetario.js
+++ js/recetario.js
@@ -3,10 +3,13 @@ const ingrediente1= prompt('Primero, ingrese un ingrediente','tomate');
 const ingrediente2= prompt('Muy bien! Ahora ingrese el segundo ingrediente','lechuga');
 const ingrediente3= prompt('Solo nos falta el tercer ingrediente','zanahoria');
 
+if(ingrediente1 == null || ingrediente2 == null || ingrediente3 == null){
+alert('que pasó?! Debian ser tres ingredientes :(')
+}else{
 document.write(`
 <h2>Su ensalada lleva los siguientes ingredientes:</h2>
 <ul class="listaIngredientes">
 <li>${ingrediente1}</li>
 <li>${ingrediente2}</li>
 <li>${ingrediente3}</li>
-</ul>`)
+</ul>`)}
